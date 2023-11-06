1eb70df44675d692e1618aef1b2959a2295189ac
shawm93 <shawnong1993@gmail.com>
2023-11-06 22:56:18 +0800

    done testing

--- get_next_line.c
+++ get_next_line.c
@@ -1,145 +1,5 @@
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	count;
-
-	count = 0;
-	while (s[count] != '\0')
-		count++;
-	return (count);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	int		i;
-	int		j;
-	char	*t;
-
-	i = 0;
-	j = -1;
-	while (s1[i])
-		i++;
-	t = malloc((i + 1) * sizeof(char));
-	if (!t)
-		return (0);
-	while (s1[++j])
-	{
-		t[j] = s1[j];
-	}
-	t[j] = '\0';
-	return (t);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	total_len;
-	char	*str;
-	int		i;
-	int		j;
-
-	if (s1 && !s2)
-		return ((char *)s1);
-	if (!s1 && s2)
-		return ((char *)s2);
-	if (!s1 && !s2)
-		return (NULL);
-	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = (char *)malloc(total_len * sizeof(char));
-	if (!str)
-		return (NULL);
-	i = -1;
-	j = -1;
-	while (s1[++i])
-		str[i] = s1[i];
-	while (s2[++j])
-		str[i++] = s2[j];
-	str[i] = '\0';
-	return (str);
-}
-
-char	*ft_strchr(char *s, int c)
-{
-	char	*ptr;
-
-	ptr = s;
-	while (*ptr)
-	{
-		if (*ptr == (unsigned char) c)
-			return (ptr + 1);
-		ptr++;
-	}
-	if (*ptr == '\0' && c == '\0')
-		return (ptr);
-	return (0);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	unsigned int	i;
-	char			*str;
-	size_t			count;
-	size_t			real;
-
-	if (!s)
-		return (NULL);
-	count = ft_strlen(s);
-	real = count;
-	if ((size_t)start >= count || len == 0)
-		return (ft_strdup(""));
-	i = 0;
-	if (count > len)
-		real = len;
-	str = (char *)malloc(sizeof(char) * (real + 1));
-	if (!str)
-		return (0);
-	while ((size_t)i < real && s[start])
-		str[i++] = s[start++];
-	str[i] = '\0';
-	return (str);
-}
-
-t_list	*ft_lstlast(t_list *lst)
-{
-	while (lst)
-	{
-		if (!(lst->next))
-			return (lst);
-		else
-			lst = lst->next;
-	}
-	return (lst);
-}
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*tmp;
-
-	if (lst && new)
-	{
-		if (*lst)
-		{
-			tmp = ft_lstlast(*lst);
-			tmp->next = new;
-		}
-		else
-			*lst = new;
-	}
-}
-
-int	ft_lstsize(t_list *lst)
-{
-	int	count;
-
-	count = 0;
-	while (lst)
-	{
-		lst = lst->next;
-		count++;
-	}
-	return (count);
-}
-
 // char *get_next_line(int fd)
 // {
 // 	size_t			bytes_read;
@@ -274,67 +134,14 @@ int	ft_lstsize(t_list *lst)
 // 	return (0);
 // }
 
-void clean_list(t_list **bufstr)
-{
-	int		i;
-	int		k;
-	t_list	*new;
-	t_list	*tmp;
-	char	*str;
-	
-	if (!bufstr || !*bufstr)
-		return ;
-	str = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
-	new = (t_list *) malloc(sizeof(t_list));
-	if (!str || !new)
-		return ;
-	tmp = ft_lstlast(*bufstr);
-	i = -1;
-	k = 0;
-	while (tmp->str[++i])
-	{
-		if (tmp->str[i] == '\n')
-			break ;
-	}
-	while (tmp->str[i++])
-		str[k++] = tmp->str[i];
-	str[k] = 0;
-	new->str = str;
-	new->next = NULL;
-	while (*bufstr)
-	{
-		tmp = (*bufstr)->next;
-		free((*bufstr)->str);
-		free(*bufstr);
-		*bufstr = tmp;
-	}
-	if (*str)
-		*bufstr = new;
-	else
-	{
-		free(str);
-		free(new);
-	}
-}
-
-char *get_line(t_list *bufstr)
+char *fill_letters(t_list *bufstr, int len)
 {
 	char	*str;
-	int		count;
-	t_list	*tmp;
 	int		i;
-	int		len;
 
-	count = ft_lstsize(bufstr);
-	tmp = ft_lstlast(bufstr);
-	i = -1;
-	while (tmp->str[++i])
-	{
-		if (tmp->str[i] == '\n')
-			break;
-	}
-	len = BUFFER_SIZE * (count - 1) + i;
 	str = (char *) malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
 	len = 0;
 	while (bufstr)
 	{
@@ -350,6 +157,25 @@ char *get_line(t_list *bufstr)
 	}
 	str[len] = '\0';
 	return(str);
+}
+
+char *get_line(t_list *bufstr)
+{
+	int		count;
+	t_list	*tmp;
+	int		i;
+	int		len;
+
+	count = ft_lstsize(bufstr);
+	tmp = ft_lstlast(bufstr);
+	i = -1;
+	while (tmp->str[++i])
+	{
+		if (tmp->str[i] == '\n')
+			break;
+	}
+	len = BUFFER_SIZE * (count - 1) + i;
+	return (fill_letters(bufstr, len));
 }
 
 int found_nl(t_list **bufstr)
@@ -408,11 +234,6 @@ char *get_next_line(int fd)
 		return (NULL);
 	str = get_line(bufstr);
 	clean_list(&bufstr);
-	// while (bufstr)
-	// {
-	// 	printf("%s\n", bufstr->str);
-	// 	bufstr = bufstr->next;
-	// }
 	return(str);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -15,6 +15,15 @@ typedef struct s_list {
     struct s_list   *next;
 }   t_list;
 
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+int	ft_lstsize(t_list *lst);
+void make_new(t_list **bufstr, char *str);
+void clean_list(t_list **bufstr);
+char *fill_letters(t_list *bufstr, int len);
+char *get_line(t_list *bufstr);
+int found_nl(t_list **bufstr);
+void create_list(int fd, t_list **bufstr);
 char *get_next_line(int fd);
 
 #endif
